544c58dba94b8f33e0436afd4fbda41a6b8f5d7f
Dona Jince <146970847+DonaJince@users.noreply.github.com>
2023-11-06 10:51:40 +0530

    Add files via upload

---
+++ postsample.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<title>A FORM</title>
+</head>
+	<body text="dodgerblue">
+		<style>
+			 form{
+			 	border:100px;
+			 }
+			  label{
+			              margin-left:775px ;
+			              display:inline-block;
+			              width:120px;
+			              }
+		</style>
+		<form action="xyz.php" method="post">
+				<label>NAME:</label><input type="text"  name="name"/><br><br>
+				<label>ROLL NO:</label><input type="text" name="roll"/><br><br>
+			<center><input type="submit" value="SUBMIT"  name="b"/></label></center>
+		</form>
+	</body>
+</html>
+
---
+++ prgm_3.php
@@ -0,0 +1,12 @@
+<html>
+	<body>
+		<center>
+			<h2><u>FIRST SESSIONAL EXAMINATION</u></h2>
+				<form method ="post" action="result.php">
+						NAME:<input type="text" name="name"><br><br>
+						ROLL NO:<input type="text" name="roll"><br><br>
+						<input type="submit" name ="sub" value="results"><br><br>
+				  </form>
+	</center>
+	</body>
+</html>
