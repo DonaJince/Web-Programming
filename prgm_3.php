851048a68eb0ebbca44626bc0811067b63e479b6
Dona Jince <146970847+DonaJince@users.noreply.github.com>
2023-11-06 11:00:30 +0530

    prgm_3.php

--- prgm_3.php
+++ prgm_3.php
@@ -2,11 +2,20 @@
 	<body>
 		<center>
 			<h2><u>FIRST SESSIONAL EXAMINATION</u></h2>
+			<table border="0">
 				<form method ="post" action="result.php">
-						NAME:<input type="text" name="name"><br><br>
-						ROLL NO:<input type="text" name="roll"><br><br>
-						<input type="submit" name ="sub" value="results"><br><br>
+				<tr>
+						<td>NAME:</td>
+						<td><input type="text" name="name"></td><br><br>
+				</tr>
+				<tr>
+						<td>ROLL NO:</td>
+						<td><input type="text" name="roll"><td><br><br>
+				<tr>
+				<tr>
+						<td  align="center" colspan="2"><input type="submit" name ="sub" value="results"></td><br><br>
 				  </form>
+				 </table>
 	</center>
 	</body>
 </html>
