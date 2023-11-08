70dbdcb4039aa6cec7c69496e3c9262ef0f08ff4
Dona Jince <146970847+DonaJince@users.noreply.github.com>
2023-11-08 10:29:19 +0530

    postsample.php

--- postsample.php
+++ postsample.php
@@ -13,11 +13,25 @@
 			              width:120px;
 			              }
 		</style>
-		<form action="xyz.php" method="post">
+		<form action="" method="post">
 				<label>NAME:</label><input type="text"  name="name"/><br><br>
 				<label>ROLL NO:</label><input type="text" name="roll"/><br><br>
 			<center><input type="submit" value="SUBMIT"  name="b"/></label></center>
 		</form>
+<?php
+	if(isset($_POST['b']))
+	{
+		if(!(empty($_POST['name']) || empty($_POST['roll'])))
+		{
+			$n =$_POST['name'];
+			$r =$_POST['roll'];
+			echo "<br><h2>my name is ",$n,"</h2><br>";
+			echo "<h2>my roll no is ",$r,"<h2>";
+		
+		}
+		else
+			echo "FILL THE FIELDS";
+	}
+?>
 	</body>
 </html>
-
