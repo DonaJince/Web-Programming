5b215085e1f948f388132d892e89dc849153e1a7
Dona Jince <146970847+DonaJince@users.noreply.github.com>
2023-11-29 11:02:00 +0530

    Insert.php

--- insert.php
+++ insert.php
@@ -1,9 +1,9 @@
 <?php
-	$con=new mysqli("localhost","root","");
+	$con=new mysqli("localhost","root","","MACE");
 	if($con->connect_error)
 		die("\nconnection failed:".$con->connect_error);
-$i="insert into student(1,'Dona','MCA')";
-if($con->query($i)===TRUE)
+$i="insert into student values(1,'Dona','MCA')";
+if($con->query($i)==TRUE)
 	echo "\ndetails inserted";
 else
 	echo "\nfailed to insert details:",$con->error;
