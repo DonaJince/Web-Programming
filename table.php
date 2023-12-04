58c0ef41d5285a460db6a20b3eab3eee96627486
DonaJince <donamaleri@gmail.com>
2023-12-04 13:49:15 +0530

    updated

--- table.php
+++ table.php
@@ -2,7 +2,8 @@
 	$con=new mysqli("localhost","root","","MACE");
 	if($con->connect_error)
 		die("\nconnection failed:".$con->connect_error);
-$s="create table student(rollno int primary key ,name varchar(20),department varchar(20) )";
+//$s="create table student(rollno int primary key ,name varchar(20),department varchar(20) )";
+$s="create table teacher(tid int primary key ,name varchar(20) )";
 if($con->query($s)===TRUE)
 	echo "\ntable created";
 else
