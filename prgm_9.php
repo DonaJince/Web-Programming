3afc633407f5c80000bfc40248cbbbd73ec60652
DonaJince <donamaleri@gmail.com>
2023-12-04 14:57:40 +0530

    updated

---
+++ prgm_9.php
@@ -0,0 +1,32 @@
+<?php
+	$con=new mysqli("localhost","root","","MACE");
+	if($con->connect_error)
+		die("\nconnection failed:".$con->connect_error);
+$s="select  *from product";
+if($con->query($s)==TRUE)
+{
+	$result=$con->query($s);
+	if($result->num_rows>0)
+	{	
+		?>
+		<table border="1">
+		<tr>
+		<th>PRODUCT ID</th>
+		<th>PRODUCT NAME</th>
+		<th>QUANTITY</th>
+		<th>UNIT PRICE</th>
+		<th>MANUFACTURED DATE</th>
+		<th>EXPIRY DATE</th>
+		</tr>
+		<?php
+		while($row=$result->fetch_assoc())
+		{
+			echo "<tr><th>".$row['pid']."</th><th>".$row['pname']."</th><th>".$row['quantity'].."</th><th>".$row['price']."</th><th>".$row['quantity']."</th><th>".$row['quantity']"</th></tr>";
+		}
+	}
+	echo "</table>";
+}
+else
+	echo "\nfailed to display details:",$con->error;
+$con->close();
+?>
