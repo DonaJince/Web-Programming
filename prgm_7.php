fd5a0819ce2b3c84afbdd22b1c8e8537a0ea4412
DonaJince <donamaleri@gmail.com>
2023-12-15 14:06:31 +0530

    updated

--- prgm_7.php
+++ prgm_7.php
@@ -2,7 +2,6 @@
 	$con=new mysqli("localhost","root","","MACE");
 	if($con->connect_error)
 		die("\nconnection failed:".$con->connect_error);
-//$s="create table student(rollno int primary key ,name varchar(20),department varchar(20) )";
 $s="create table product(pid int primary key AUTO_INCREMENT ,pname varchar(20),quantity int,price varchar(20),man_date  date,exp_date date)";
 if($con->query($s)===TRUE)
 	echo "\ntable created";
